acab9cb5debb2b0a845c2dd18f19a102e5554f0f
Daniel H <daguiheso@gmail.com>
2020-02-13 17:36:33 -0500

    add file-loader for handle images with webpack

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import '../assets/styles/components/CarouselItem.scss';
+import playIcon from '../assets/images/play-icon.png';
+import plusIcon from '../assets/images/plus-icon.png';
 
 const CarouselItem = ({ children }) => (
   <div className='carousel-item'>
     <img className='carousel-item__img' src='https://images.pexels.com/photos/1686657/pexels-photo-1686657.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' alt='Tech' />
     <div className='carousel-item__details'>
       <div>
-        <img src='/src/assets/images/play-icon.png' alt='Play' />
-        <img src='/src/assets/images/plus-icon.png' alt='Plus' />
+        <img src={playIcon} alt='Play' />
+        <img src={plusIcon} alt='Plus' />
       </div>
       <p className='carousel-item__title'>Titulo descriptivo</p>
       <p className='carousel-item__subtitle'>2019 16+ 114 min</p>
