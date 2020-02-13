4c1cebae457f400e7b037309c7f6f3b17d25f1e9
Daniel H <daguiheso@gmail.com>
2020-02-13 15:50:25 -0500

    create Category component

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
+import Category from '../components/Category';
 import '../assets/styles/App.scss';
 
 const App = () => (
   <div className='App'>
     <Header />
     <Search />
+    <Category />
   </div>
 );
 
