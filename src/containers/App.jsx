f5961f93f6f17332651fe4519cfedc864a324249
Daniel H <daguiheso@gmail.com>
2020-02-14 10:34:29 -0500

    add react hooks for videos

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Category from '../components/Category';
@@ -8,34 +8,44 @@ import Footer from '../components/Footer';
 
 import '../assets/styles/App.scss';
 
-const App = () => (
-  <div className='App'>
-    <Header />
-    <Search />
-    <Category title='Mi Lista'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Category>
+const App = () => {
+  const [videos, setVideos] = useState([]);
 
-    <Category title='Tendencias'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Category>
+  useEffect(() => {
+    fetch('http://localhost:3000/initalState')
+      .then((response) => response.json())
+      .then((data) => setVideos(data));
+  }, []);
 
-    <Category title='Proximos'>
-      <Carousel>
-        <CarouselItem />
-      </Carousel>
-    </Category>
-    <Footer />
-  </div>
-);
+  return (
+    <div className='App'>
+      <Header />
+      <Search />
+      <Category title='Mi Lista'>
+        <Carousel>
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+        </Carousel>
+      </Category>
+
+      <Category title='Tendencias'>
+        <Carousel>
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+        </Carousel>
+      </Category>
+
+      <Category title='Proximos'>
+        <Carousel>
+          <CarouselItem />
+        </Carousel>
+      </Category>
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
