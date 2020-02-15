7404c2e54c5bed8069b96ea937c2141c6241d255
Daniel H <daguiheso@gmail.com>
2020-02-14 19:52:36 -0500

    create Login component

---
+++ src/containers/Login.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import googleIcon from '../assets/images/google-icon.png';
+import twitterIcon from '../assets/images/twitter-icon.png';
+
+import '../assets/styles/components/Login.scss';
+
+const Login = () => (
+  <section className='login'>
+    <section className='login__container'>
+      <h2>Inicia sesión</h2>
+      <form className='login__container--form'>
+        <input className='input' type='text' placeholder='Correo' />
+        <input className='input' type='password' placeholder='Contraseña' />
+        <button className='button' type='button'>
+          Iniciar sesión
+        </button>
+        <div className='login__container--remember-me'>
+          <label>
+            <input type='checkbox' id='cbox1' value='first_checkbox' />
+            Recuérdame
+          </label>
+          <a href='/'>Olvidé mi contraseña</a>
+        </div>
+      </form>
+      <section className='login__container--social-media'>
+        <div>
+          <img src={googleIcon} alt='google' />
+          Inicia sesión con Google
+        </div>
+        <div>
+          <img src={twitterIcon} alt='twitter' />
+          Inicia sesión con Twitter
+        </div>
+      </section>
+      <p className='login__container--register'>
+        No tienes ninguna cuenta
+        <a href='/register'>Regístrate</a>
+      </p>
+    </section>
+  </section>
+);
+
+export default Login;
