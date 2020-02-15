5ad138e247c7f11054333306ea5e7b3b3f2312d1
Daniel H <daguiheso@gmail.com>
2020-02-14 20:42:48 -0500

    implemented Layout component on all routes

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import Header from '../components/Header';
 import Search from '../components/Search';
 import Category from '../components/Category';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import Footer from '../components/Footer';
 
 import useInitialState from '../hooks/useInitialState';
 
@@ -15,8 +13,7 @@ const API = 'http://localhost:3000/initalState';
 const Home = () => {
   const initialState = useInitialState(API);
   return (
-    <div className='App'>
-      <Header />
+    <>
       <Search />
       { initialState.mylist.length > 0 && (
         <Category title='Mi Lista'>
@@ -43,8 +40,7 @@ const Home = () => {
           ))}
         </Carousel>
       </Category>
-      <Footer />
-    </div>
+    </>
   );
 };
 
