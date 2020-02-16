e2e2421553006ff807f1fbe4a155847458c61b56
Daniel H <daguiheso@gmail.com>
2020-02-16 10:49:34 -0500

    implement classNames for Layout component with location validate

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+import classNames from 'classnames';
+
 import Header from './Header';
 import Footer from './Footer';
-import '../assets/styles/components/NotFound.scss';
+import '../assets/styles/components/Layout.scss';
 
-const Layout = ({ children }) => (
-  <div className='App'>
-    <Header />
-    {children}
-    <Footer />
-  </div>
-);
+const Layout = withRouter(({ children, location }) => {
+  const isHome = location.pathname === '/';
+  const appStyle = classNames('app', { isHome });
+  return (
+    <div className={appStyle}>
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+});
 
 export default Layout;
