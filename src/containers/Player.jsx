aa60541283e7a9920b4474c06b9631df0be62935
Daniel H <daguiheso@gmail.com>
2020-02-15 23:40:05 -0500

    logic goBack for button on /player

--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import '../assets/styles/components/Player.scss';
 
-const Player = () => {
+const Player = (props) => {
+  const { history } = props;
   return (
     <div className='player'>
       <video controls autoPlay>
         <source src='' type='video/mp4' />
       </video>
       <div className='player__back'>
-        <button type='button'>Regresar</button>
+        <button type='button' onClick={() => history.goBack()}>Regresar</button>
       </div>
     </div>
   );
