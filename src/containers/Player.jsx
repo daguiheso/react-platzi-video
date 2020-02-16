d507615d07f8f5143548df81a8f3c0f31ecad89c
Daniel H <daguiheso@gmail.com>
2020-02-16 09:44:41 -0500

    implemented getVideoSource action and get playing from store

--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -1,18 +1,40 @@
-import React from 'react';
+/* eslint-disable react/destructuring-assignment */
+import React, { useLayoutEffect } from 'react';
+import { connect } from 'react-redux';
+import NotFound from '../components/NotFound';
+import { getVideoSource } from '../actions';
+
 import '../assets/styles/components/Player.scss';
 
 const Player = (props) => {
   const { history } = props;
-  return (
+  const { id } = props.match.params;
+  const hasPlaying = Object.keys(props.playing).length > 0;
+
+  useLayoutEffect(() => {
+    props.getVideoSource(id);
+  }, []);
+
+  return hasPlaying ? (
     <div className='player'>
       <video controls autoPlay>
-        <source src='' type='video/mp4' />
+        <source src={props.playing.source} type='video/mp4' />
       </video>
       <div className='player__back'>
         <button type='button' onClick={() => history.goBack()}>Regresar</button>
       </div>
     </div>
-  );
+  ) : <NotFound />;
 };
 
-export default Player;
+const mapStateToProps = (state) => {
+  return {
+    playing: state.playing,
+  };
+};
+
+const mapDispatchToProps = {
+  getVideoSource,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Player);
