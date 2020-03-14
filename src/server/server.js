5b9db28898aba3c3630ce20e13a87bfa53c93bc5
Daniel H <daguiheso@gmail.com>
2020-03-13 21:32:03 -0500

    config dotenv

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,20 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { ENV, PORT } = process.env;
 
 const app = express();
+
+if (ENV === 'development') {
+  console.log('Development config');
+}
 
 app.get('*', (req, res) => {
   res.send({ hello: 'express' });
 });
 
-app.listen(3000, (error) => {
+app.listen(PORT, (error) => {
   error ? console.log(error) : console.log('Server running on 3000 port');
 });
