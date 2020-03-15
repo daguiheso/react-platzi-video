2edb3252479358f7deb67817bd99e28e8e4c5077
Daniel H <daguiheso@gmail.com>
2020-03-14 21:53:50 -0500

    server side rendering

--- src/server/server.js
+++ src/server/server.js
@@ -3,6 +3,16 @@ import express from 'express';
 import dotenv from 'dotenv';
 import webpack from 'webpack';
 
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { Provider } from 'react-redux';
+import { createStore, compose } from 'redux';
+import { renderRoutes } from 'react-router-config';
+import { StaticRouter } from 'react-router-dom';
+import reducer from '../frontend/reducers';
+import initialState from '../frontend/initialState';
+import serverRoutes from '../frontend/routes/serverRoutes';
+
 dotenv.config();
 
 const { ENV, PORT } = process.env;
@@ -24,8 +34,8 @@ if (ENV === 'development') {
   app.use(webpackHotMiddleware(compiler));
 }
 
-app.get('*', (req, res) => {
-  res.send(`
+const setResponse = html => {
+  return (`
     <!DOCTYPE html>
     <html lang="en">
       <head>
@@ -36,12 +46,27 @@ app.get('*', (req, res) => {
         <title>Document</title>
       </head>
       <body>
-        <div id="app"></div>
+        <div id="app">${html}</div>
         <script src="assets/app.js" type="text/javascript"></script>
       </body>
     </html>
   `);
-});
+};
+
+const renderApp = (req, res) => {
+  const store = createStore(reducer, initialState);
+  const html = renderToString(
+    <Provider store={store}>
+      <StaticRouter location={req.url} context={{}}>
+        {renderRoutes(serverRoutes)}
+      </StaticRouter>
+    </Provider>,
+  );
+
+  res.send(setResponse(html));
+};
+
+app.get('*', renderApp);
 
 app.listen(PORT, (error) => {
   error ? console.log(error) : console.log('Server running on 3000 port');
