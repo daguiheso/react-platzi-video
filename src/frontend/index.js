82b5562bacb20c4551dd5a2357d763d1882c3c98
Daniel H <daguiheso@gmail.com>
2020-03-14 21:27:31 -0500

    implemented history for frontend app

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, compose } from 'redux';
+import { createBrowserHistory } from 'history';
+import { Router } from 'react-router-dom';
 import reducer from './reducers';
 import App from './routes/App';
 
@@ -171,12 +173,15 @@ const initialState = {
   ],
 };
 
+const history = createBrowserHistory();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, initialState, composeEnhancers());
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history={history}>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('app'),
 );
