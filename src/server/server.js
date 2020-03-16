22b5dc3551e451162adb40500380f9a367eaac37
Daniel H <daguiheso@gmail.com>
2020-03-16 10:29:38 -0500

    apply hashes to the name of our builds to invalidate cache

--- src/server/server.js
+++ src/server/server.js
@@ -14,6 +14,7 @@ import reducer from '../frontend/reducers';
 import initialState from '../frontend/initialState';
 import serverRoutes from '../frontend/routes/serverRoutes';
 import Layout from '../frontend/components/Layout';
+import getManifest from './getManifest';
 
 dotenv.config();
 
@@ -35,13 +36,21 @@ if (ENV === 'development') {
   app.use(webpackDevMiddleware(compiler, serverConfig));
   app.use(webpackHotMiddleware(compiler));
 } else {
+  app.use((req, res, next) => {
+    if (!req.hashManifest) req.hashManifest = getManifest();
+    next();
+  });
   app.use(express.static(`${__dirname}/public`));
   app.use(helmet());
   app.use(helmet.permittedCrossDomainPolicies());
-  app.disable('x-powered-by')
+  app.disable('x-powered-by');
 };
 
-const setResponse = (html, preloadedState) => {
+const setResponse = (html, preloadedState, manifest) => {
+  const mainStyles = manifest ? manifest['main.css'] : 'assets/app.css';
+  const mainBuild = manifest ? manifest['main.js'] : 'assets/app.js';
+
+
   return (`
     <!DOCTYPE html>
     <html lang="en">
@@ -49,7 +58,7 @@ const setResponse = (html, preloadedState) => {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="assets/app.css" type="text/css">
+        <link rel="stylesheet" href="${mainStyles}" type="text/css">
         <title>Document</title>
       </head>
       <body>
@@ -58,7 +67,7 @@ const setResponse = (html, preloadedState) => {
           window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')};
           window.__MAMA__ = 'que pasa wey!';
         </script>
-        <script src="assets/app.js" type="text/javascript"></script>
+        <script src="${mainBuild}" type="text/javascript"></script>
       </body>
     </html>
   `);
@@ -77,7 +86,7 @@ const renderApp = (req, res) => {
     </Provider>,
   );
 
-  res.send(setResponse(html, preloadedState));
+  res.send(setResponse(html, preloadedState, req.hashManifest));
 };
 
 app.get('*', renderApp);
